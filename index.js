f1fbc49a4809781c275d31d6c84f91ce3d492c66
midhuna0003-sudo <midhuna0003@gmail.com>
2025-09-19 12:47:42 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,76 +3,91 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "ROOT_CONFIG_FILENAMES", {
+exports.createConfigItem = createConfigItem;
+exports.createConfigItemAsync = createConfigItemAsync;
+exports.createConfigItemSync = createConfigItemSync;
+Object.defineProperty(exports, "default", {
   enumerable: true,
   get: function () {
-    return _configuration.ROOT_CONFIG_FILENAMES;
+    return _full.default;
   }
 });
-Object.defineProperty(exports, "findConfigUpwards", {
-  enumerable: true,
-  get: function () {
-    return _configuration.findConfigUpwards;
+exports.loadOptions = loadOptions;
+exports.loadOptionsAsync = loadOptionsAsync;
+exports.loadOptionsSync = loadOptionsSync;
+exports.loadPartialConfig = loadPartialConfig;
+exports.loadPartialConfigAsync = loadPartialConfigAsync;
+exports.loadPartialConfigSync = loadPartialConfigSync;
+function _gensync() {
+  const data = require("gensync");
+  _gensync = function () {
+    return data;
+  };
+  return data;
+}
+var _full = require("./full.js");
+var _partial = require("./partial.js");
+var _item = require("./item.js");
+var _rewriteStackTrace = require("../errors/rewrite-stack-trace.js");
+const loadPartialConfigRunner = _gensync()(_partial.loadPartialConfig);
+function loadPartialConfigAsync(...args) {
+  return (0, _rewriteStackTrace.beginHiddenCallStack)(loadPartialConfigRunner.async)(...args);
+}
+function loadPartialConfigSync(...args) {
+  return (0, _rewriteStackTrace.beginHiddenCallStack)(loadPartialConfigRunner.sync)(...args);
+}
+function loadPartialConfig(opts, callback) {
+  if (callback !== undefined) {
+    (0, _rewriteStackTrace.beginHiddenCallStack)(loadPartialConfigRunner.errback)(opts, callback);
+  } else if (typeof opts === "function") {
+    (0, _rewriteStackTrace.beginHiddenCallStack)(loadPartialConfigRunner.errback)(undefined, opts);
+  } else {
+    {
+      return loadPartialConfigSync(opts);
+    }
   }
-});
-Object.defineProperty(exports, "findPackageData", {
-  enumerable: true,
-  get: function () {
-    return _package.findPackageData;
+}
+function* loadOptionsImpl(opts) {
+  var _config$options;
+  const config = yield* (0, _full.default)(opts);
+  return (_config$options = config == null ? void 0 : config.options) != null ? _config$options : null;
+}
+const loadOptionsRunner = _gensync()(loadOptionsImpl);
+function loadOptionsAsync(...args) {
+  return (0, _rewriteStackTrace.beginHiddenCallStack)(loadOptionsRunner.async)(...args);
+}
+function loadOptionsSync(...args) {
+  return (0, _rewriteStackTrace.beginHiddenCallStack)(loadOptionsRunner.sync)(...args);
+}
+function loadOptions(opts, callback) {
+  if (callback !== undefined) {
+    (0, _rewriteStackTrace.beginHiddenCallStack)(loadOptionsRunner.errback)(opts, callback);
+  } else if (typeof opts === "function") {
+    (0, _rewriteStackTrace.beginHiddenCallStack)(loadOptionsRunner.errback)(undefined, opts);
+  } else {
+    {
+      return loadOptionsSync(opts);
+    }
   }
-});
-Object.defineProperty(exports, "findRelativeConfig", {
-  enumerable: true,
-  get: function () {
-    return _configuration.findRelativeConfig;
+}
+const createConfigItemRunner = _gensync()(_item.createConfigItem);
+function createConfigItemAsync(...args) {
+  return (0, _rewriteStackTrace.beginHiddenCallStack)(createConfigItemRunner.async)(...args);
+}
+function createConfigItemSync(...args) {
+  return (0, _rewriteStackTrace.beginHiddenCallStack)(createConfigItemRunner.sync)(...args);
+}
+function createConfigItem(target, options, callback) {
+  if (callback !== undefined) {
+    (0, _rewriteStackTrace.beginHiddenCallStack)(createConfigItemRunner.errback)(target, options, callback);
+  } else if (typeof options === "function") {
+    (0, _rewriteStackTrace.beginHiddenCallStack)(createConfigItemRunner.errback)(target, undefined, callback);
+  } else {
+    {
+      return createConfigItemSync(target, options);
+    }
   }
-});
-Object.defineProperty(exports, "findRootConfig", {
-  enumerable: true,
-  get: function () {
-    return _configuration.findRootConfig;
-  }
-});
-Object.defineProperty(exports, "loadConfig", {
-  enumerable: true,
-  get: function () {
-    return _configuration.loadConfig;
-  }
-});
-Object.defineProperty(exports, "loadPlugin", {
-  enumerable: true,
-  get: function () {
-    return _plugins.loadPlugin;
-  }
-});
-Object.defineProperty(exports, "loadPreset", {
-  enumerable: true,
-  get: function () {
-    return _plugins.loadPreset;
-  }
-});
-Object.defineProperty(exports, "resolvePlugin", {
-  enumerable: true,
-  get: function () {
-    return _plugins.resolvePlugin;
-  }
-});
-Object.defineProperty(exports, "resolvePreset", {
-  enumerable: true,
-  get: function () {
-    return _plugins.resolvePreset;
-  }
-});
-Object.defineProperty(exports, "resolveShowConfigPath", {
-  enumerable: true,
-  get: function () {
-    return _configuration.resolveShowConfigPath;
-  }
-});
-var _package = require("./package.js");
-var _configuration = require("./configuration.js");
-var _plugins = require("./plugins.js");
-({});
+}
 0 && 0;
 
 //# sourceMappingURL=index.js.map
--- plugins.js
+++ plugins.js
@@ -3,214 +3,64 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.loadPlugin = loadPlugin;
-exports.loadPreset = loadPreset;
-exports.resolvePreset = exports.resolvePlugin = void 0;
-function _debug() {
-  const data = require("debug");
-  _debug = function () {
-    return data;
-  };
-  return data;
-}
-function _path() {
-  const data = require("path");
-  _path = function () {
-    return data;
-  };
-  return data;
-}
-var _async = require("../../gensync-utils/async.js");
-var _moduleTypes = require("./module-types.js");
-function _url() {
-  const data = require("url");
-  _url = function () {
-    return data;
-  };
-  return data;
-}
-var _importMetaResolve = require("../../vendor/import-meta-resolve.js");
-function _fs() {
-  const data = require("fs");
-  _fs = function () {
-    return data;
-  };
-  return data;
-}
-const debug = _debug()("babel:config:loading:files:plugins");
-const EXACT_RE = /^module:/;
-const BABEL_PLUGIN_PREFIX_RE = /^(?!@|module:|[^/]+\/|babel-plugin-)/;
-const BABEL_PRESET_PREFIX_RE = /^(?!@|module:|[^/]+\/|babel-preset-)/;
-const BABEL_PLUGIN_ORG_RE = /^(@babel\/)(?!plugin-|[^/]+\/)/;
-const BABEL_PRESET_ORG_RE = /^(@babel\/)(?!preset-|[^/]+\/)/;
-const OTHER_PLUGIN_ORG_RE = /^(@(?!babel\/)[^/]+\/)(?![^/]*babel-plugin(?:-|\/|$)|[^/]+\/)/;
-const OTHER_PRESET_ORG_RE = /^(@(?!babel\/)[^/]+\/)(?![^/]*babel-preset(?:-|\/|$)|[^/]+\/)/;
-const OTHER_ORG_DEFAULT_RE = /^(@(?!babel$)[^/]+)$/;
-const resolvePlugin = exports.resolvePlugin = resolveStandardizedName.bind(null, "plugin");
-const resolvePreset = exports.resolvePreset = resolveStandardizedName.bind(null, "preset");
-function* loadPlugin(name, dirname) {
-  const filepath = resolvePlugin(name, dirname, yield* (0, _async.isAsync)());
-  const value = yield* requireModule("plugin", filepath);
-  debug("Loaded plugin %o from %o.", name, dirname);
-  return {
-    filepath,
-    value
-  };
-}
-function* loadPreset(name, dirname) {
-  const filepath = resolvePreset(name, dirname, yield* (0, _async.isAsync)());
-  const value = yield* requireModule("preset", filepath);
-  debug("Loaded preset %o from %o.", name, dirname);
-  return {
-    filepath,
-    value
-  };
-}
-function standardizeName(type, name) {
-  if (_path().isAbsolute(name)) return name;
-  const isPreset = type === "preset";
-  return name.replace(isPreset ? BABEL_PRESET_PREFIX_RE : BABEL_PLUGIN_PREFIX_RE, `babel-${type}-`).replace(isPreset ? BABEL_PRESET_ORG_RE : BABEL_PLUGIN_ORG_RE, `$1${type}-`).replace(isPreset ? OTHER_PRESET_ORG_RE : OTHER_PLUGIN_ORG_RE, `$1babel-${type}-`).replace(OTHER_ORG_DEFAULT_RE, `$1/babel-${type}`).replace(EXACT_RE, "");
-}
-function* resolveAlternativesHelper(type, name) {
-  const standardizedName = standardizeName(type, name);
-  const {
-    error,
-    value
-  } = yield standardizedName;
-  if (!error) return value;
-  if (error.code !== "MODULE_NOT_FOUND") throw error;
-  if (standardizedName !== name && !(yield name).error) {
-    error.message += `\n- If you want to resolve "${name}", use "module:${name}"`;
-  }
-  if (!(yield standardizeName(type, "@babel/" + name)).error) {
-    error.message += `\n- Did you mean "@babel/${name}"?`;
-  }
-  const oppositeType = type === "preset" ? "plugin" : "preset";
-  if (!(yield standardizeName(oppositeType, name)).error) {
-    error.message += `\n- Did you accidentally pass a ${oppositeType} as a ${type}?`;
-  }
-  if (type === "plugin") {
-    const transformName = standardizedName.replace("-proposal-", "-transform-");
-    if (transformName !== standardizedName && !(yield transformName).error) {
-      error.message += `\n- Did you mean "${transformName}"?`;
+exports.validatePluginObject = validatePluginObject;
+var _optionAssertions = require("./option-assertions.js");
+const VALIDATORS = {
+  name: _optionAssertions.assertString,
+  manipulateOptions: _optionAssertions.assertFunction,
+  pre: _optionAssertions.assertFunction,
+  post: _optionAssertions.assertFunction,
+  inherits: _optionAssertions.assertFunction,
+  visitor: assertVisitorMap,
+  parserOverride: _optionAssertions.assertFunction,
+  generatorOverride: _optionAssertions.assertFunction
+};
+function assertVisitorMap(loc, value) {
+  const obj = (0, _optionAssertions.assertObject)(loc, value);
+  if (obj) {
+    Object.keys(obj).forEach(prop => {
+      if (prop !== "_exploded" && prop !== "_verified") {
+        assertVisitorHandler(prop, obj[prop]);
+      }
+    });
+    if (obj.enter || obj.exit) {
+      throw new Error(`${(0, _optionAssertions.msg)(loc)} cannot contain catch-all "enter" or "exit" handlers. Please target individual nodes.`);
     }
   }
-  error.message += `\n
-Make sure that all the Babel plugins and presets you are using
-are defined as dependencies or devDependencies in your package.json
-file. It's possible that the missing plugin is loaded by a preset
-you are using that forgot to add the plugin to its dependencies: you
-can workaround this problem by explicitly adding the missing package
-to your top-level package.json.
-`;
-  throw error;
+  return obj;
 }
-function tryRequireResolve(id, dirname) {
-  try {
-    if (dirname) {
-      return {
-        error: null,
-        value: (((v, w) => (v = v.split("."), w = w.split("."), +v[0] > +w[0] || v[0] == w[0] && +v[1] >= +w[1]))(process.versions.node, "8.9") ? require.resolve : (r, {
-          paths: [b]
-        }, M = require("module")) => {
-          let f = M._findPath(r, M._nodeModulePaths(b).concat(b));
-          if (f) return f;
-          f = new Error(`Cannot resolve module '${r}'`);
-          f.code = "MODULE_NOT_FOUND";
-          throw f;
-        })(id, {
-          paths: [dirname]
-        })
+function assertVisitorHandler(key, value) {
+  if (value && typeof value === "object") {
+    Object.keys(value).forEach(handler => {
+      if (handler !== "enter" && handler !== "exit") {
+        throw new Error(`.visitor["${key}"] may only have .enter and/or .exit handlers.`);
+      }
+    });
+  } else if (typeof value !== "function") {
+    throw new Error(`.visitor["${key}"] must be a function`);
+  }
+}
+function validatePluginObject(obj) {
+  const rootPath = {
+    type: "root",
+    source: "plugin"
+  };
+  Object.keys(obj).forEach(key => {
+    const validator = VALIDATORS[key];
+    if (validator) {
+      const optLoc = {
+        type: "option",
+        name: key,
+        parent: rootPath
       };
+      validator(optLoc, obj[key]);
     } else {
-      return {
-        error: null,
-        value: require.resolve(id)
-      };
+      const invalidPluginPropertyError = new Error(`.${key} is not a valid Plugin property`);
+      invalidPluginPropertyError.code = "BABEL_UNKNOWN_PLUGIN_PROPERTY";
+      throw invalidPluginPropertyError;
     }
-  } catch (error) {
-    return {
-      error,
-      value: null
-    };
-  }
-}
-function tryImportMetaResolve(id, options) {
-  try {
-    return {
-      error: null,
-      value: (0, _importMetaResolve.resolve)(id, options)
-    };
-  } catch (error) {
-    return {
-      error,
-      value: null
-    };
-  }
-}
-function resolveStandardizedNameForRequire(type, name, dirname) {
-  const it = resolveAlternativesHelper(type, name);
-  let res = it.next();
-  while (!res.done) {
-    res = it.next(tryRequireResolve(res.value, dirname));
-  }
-  return res.value;
-}
-function resolveStandardizedNameForImport(type, name, dirname) {
-  const parentUrl = (0, _url().pathToFileURL)(_path().join(dirname, "./babel-virtual-resolve-base.js")).href;
-  const it = resolveAlternativesHelper(type, name);
-  let res = it.next();
-  while (!res.done) {
-    res = it.next(tryImportMetaResolve(res.value, parentUrl));
-  }
-  return (0, _url().fileURLToPath)(res.value);
-}
-function resolveStandardizedName(type, name, dirname, resolveESM) {
-  if (!_moduleTypes.supportsESM || !resolveESM) {
-    return resolveStandardizedNameForRequire(type, name, dirname);
-  }
-  try {
-    const resolved = resolveStandardizedNameForImport(type, name, dirname);
-    if (!(0, _fs().existsSync)(resolved)) {
-      throw Object.assign(new Error(`Could not resolve "${name}" in file ${dirname}.`), {
-        type: "MODULE_NOT_FOUND"
-      });
-    }
-    return resolved;
-  } catch (e) {
-    try {
-      return resolveStandardizedNameForRequire(type, name, dirname);
-    } catch (e2) {
-      if (e.type === "MODULE_NOT_FOUND") throw e;
-      if (e2.type === "MODULE_NOT_FOUND") throw e2;
-      throw e;
-    }
-  }
-}
-{
-  var LOADING_MODULES = new Set();
-}
-function* requireModule(type, name) {
-  {
-    if (!(yield* (0, _async.isAsync)()) && LOADING_MODULES.has(name)) {
-      throw new Error(`Reentrant ${type} detected trying to load "${name}". This module is not ignored ` + "and is trying to load itself while compiling itself, leading to a dependency cycle. " + 'We recommend adding it to your "ignore" list in your babelrc, or to a .babelignore.');
-    }
-  }
-  try {
-    {
-      LOADING_MODULES.add(name);
-    }
-    {
-      return yield* (0, _moduleTypes.default)(name, `You appear to be using a native ECMAScript module ${type}, ` + "which is only supported when running Babel asynchronously.", true);
-    }
-  } catch (err) {
-    err.message = `[BABEL]: ${err.message} (While processing: ${name})`;
-    throw err;
-  } finally {
-    {
-      LOADING_MODULES.delete(name);
-    }
-  }
+  });
+  return obj;
 }
 0 && 0;
 
